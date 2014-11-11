8c4072ce830d59f9bf53eea2ba1adc431aab80f3
Rohan <rohan.parikh21@gmail.com>
2014-11-10 23:35:34 -0500

    Modified reducer for frequency counting

--- src/SumReducer.java
+++ src/SumReducer.java
@@ -1,9 +1,12 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+// For understanding purposes, read the paragraph listed below. Helped me a lot :)
 /* 
  * To define a reduce function for your MapReduce job, subclass 
  * the Reducer class and override the reduce method.
@@ -16,7 +19,7 @@ import org.apache.hadoop.mapreduce.Reducer;
  *   The data type of the output value
  */   
 public class SumReducer extends Reducer<Text, Text, Text, Text> {
-
+// Also this paragraph ...
   /*
    * The reduce method runs once for each key received from
    * the shuffle and sort phase of the MapReduce framework.
@@ -27,37 +30,33 @@ public class SumReducer extends Reducer<Text, Text, Text, Text> {
 	public void reduce(Text key, Iterable<Text> values, Context context)
 			throws IOException, InterruptedException {
 		
-		/*
-		 * For each value in the set of values passed to us by the mapper:
-		 */
-		
-		String reduced = "";
-		Text previous=values(0);
-		int count=0;
-		for (Text value : values) {
-		  
-		  /*
-		   * Add the value to the word count counter for this key.
-		   */if(previous!=value)
-		   {
-		   	count++;
-		   	previous=value;
-		   }
-		   else
-		   {
-		   	reduced = reduced + " " +previous.toString() + " " +  count;
-		   	count=1;
-		   	previous=value;
-		   }
-			
-		}
-		reduced = reduced + " " +previous.toString() + " " +  count;
-		System.out.println(key.toString());
-		System.out.println(reduced);
-		/*
-		 * Call the write method on the Context object to emit a key
-		 * and a value from the reduce method. 
-		 */
-		context.write(key, new Text(reduced));
-	}
+	  String reduced = "";  //This variable stores the concatenated list of URL's
+	  String previous = "";
+	  ArrayList<String> a = new ArrayList<String>();
+	  for(Text value : values){
+		  a.add(value.toString());
+	  }
+	  Collections.sort(a);  //Sorting the list for link frequency
+	  previous = a.get(0);
+	  int count = 0;
+	  for(int i=0; i<a.size(); i++){
+		  if(a.get(i) == previous){
+			  count++;
+			  previous = a.get(i);
+		  }
+		  else{
+			  reduced = reduced + " " + previous + " "+count;
+			  count = 1;
+			  previous = a.get(i);
+		  }
+	  }
+	  // For the list URL to be added 
+	  reduced = reduced + " " +previous.toString() + " " +  count;
+	  // For debugging purposes, go to http://localhost:50030/jobtracker.jsp
+	  System.out.println(key.toString());
+	  System.out.println(reduced);
+	  // Call the write method on the Context object to emit a key and a value from the reduce method. 
+	  context.write(key, new Text(reduced));
+  }
 }
+
